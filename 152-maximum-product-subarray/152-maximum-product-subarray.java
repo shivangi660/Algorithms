cf3e27a0a4e98e4a01479108f97dddb41c9761a6
Shivangi Sharma <shivangi660@gmail.com>
2022-04-12 10:49:54 +0100

    Time: 2 ms (71.82%), Space: 45 MB (39.15%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,19 +1,35 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        // This is valid when there is no 0 in the array
-        // checked from constraints.
-        // So that means prefix sum will either begin at 0 or end at n-1.
+        // The maxProduct will either start from 0 or it will end at n-1
+        // So catch is to compute prefix and the suffix sum and check the max value.
         
-        int prefix = 0, suffix = 0, ans = nums[0];
-        for(int i = 0; i < nums.length; i++){
-            prefix = (prefix == 0 ? 1 : prefix) * nums[i];
-            suffix = (suffix == 0 ? 1 : suffix) * nums[nums.length - 1 - i];
-            ans = Math.max(ans, Math.max(prefix, suffix));
+        // int prefix = 0, suffix = 0, ans = nums[0];
+        // for(int i = 0; i < nums.length; i++){
+        //     prefix = (prefix == 0 ? 1 : prefix) * nums[i];
+        //     suffix = (suffix == 0 ? 1 : suffix) * nums[nums.length - 1 - i];
+        //     ans = Math.max(ans, Math.max(prefix, suffix));
+        // }
+        // return ans;
+        
+        // Other solution is to have min and max and interchange it whenever a negative value is encountered.
+        
+        
+        
+        int min = nums[0], max = nums[0], ans = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < 0){
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            
+            min = Math.min(min*nums[i], nums[i]);
+            max = Math.max(max*nums[i], nums[i]);
+            ans = Math.max(ans, max);
+            
         }
         return ans;
-        
-        
         
     }
     
