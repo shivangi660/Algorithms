8428fda195e1ac2b99054aecbdd67ee06e30b6b6
Shivangi Sharma <shivangi660@gmail.com>
2022-04-19 01:24:09 +0100

    Time: 23 ms (29.13%), Space: 41.7 MB (94.52%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   
+    public int pathSum(TreeNode root, int targetSum) {
+        // The mistake done was:
+        //pathSumHelper(root.left,targetSum) + pathSumHelper(root.left, targetSum - value)
+        //There was the miss that when in the recursive function the targetSum will be called, it won't be called with the targetSum value but will be called with the updated value.
+        
+        // ****** Right Recursive Solution Here will be ******
+    
+        if(root == null){
+            return 0;
+        }
+        return pathSumHelper(root,targetSum) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+        
+        public int pathSumHelper(TreeNode node, int currentSum){
+            if(node == null){
+                return 0;
+            }
+            return (currentSum == node.val ? 1 : 0) + pathSumHelper(node.left, currentSum - node.val) +                                                    pathSumHelper(node.right, currentSum - node.val);
+        }
+}
