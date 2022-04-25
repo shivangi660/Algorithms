ad7b12ea2f1134ae57f5889955184e48739d36cf
Shivangi Sharma <shivangi660@gmail.com>
2022-04-25 15:47:31 +0100

    Time: 474 ms (47.63%), Space: 49.8 MB (75.13%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minKnightMoves(int x, int y) {
+        int maxLength = 601;
+        Deque<int[]> queue = new LinkedList<>();
+        
+        int[][] directions = {{1,2}, {1,-2}, {2,1}, {2,-1}, {-1,2}, {-2,1}, {-1,-2},{-2,-1}};
+        int base = 300;
+        int[][] visited = new int[maxLength][maxLength];
+        
+        
+        queue.add(new int[]{0,0});
+        int count = 0;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int[] point = queue.poll();
+                int r = point[0], c = point[1];
+                if(visited[r + base][c + base] == 1){
+                    continue;
+                }
+                visited[r + base][c + base] = 1;
+                for(int[] dir : directions){
+                    queue.offer(new int[]{r + dir[0], c + dir[1]});
+                } 
+                if(visited[x + base][y + base] == 1)
+                    return count;
+           }
+            count++; 
+        }
+        return -1; 
+        
+    }
+}
